aae2ec2585507bde0da1511d5eef6d80614440c7
gokul-00 <ygokul1702@gmail.com>
2020-05-24 15:04:16 +0530

    changed max ratio to 0.35

--- collision.js
+++ collision.js
@@ -239,7 +239,7 @@ interval = setInterval(function() {
                 }
             }
         }
-    if(ratio <= 0.40){
+    if(ratio <= 0.35){
         item = particles.length;
         circleArea+=Math.PI*radius*radius;
         particles.push(new Particle(x, y, radius, item));
